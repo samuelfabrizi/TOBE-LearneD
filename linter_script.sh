8d296bb5b2e27092e2f5f38e21c8f85f4409ad68
Samuel Fabrizi <samuel.fabrizi97@gmail.com>
2021-10-03 18:52:44 +0200

    fix: removed utils for linting and coverage

--- linter_script.sh
+++ linter_script.sh
@@ -3,4 +3,4 @@
 pip install -r requirements/requirements.txt
 pip install -r requirements/requirements-test.txt
 
-pylint --fail-under=9 --ignore-patterns=test* --disable=W1514 decentralized_smart_grid_ml/
+pylint --fail-under=9 --ignore-patterns=test* --disable=W1514,E1101 decentralized_smart_grid_ml/
