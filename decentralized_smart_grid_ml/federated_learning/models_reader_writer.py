4df85ee97b208aae817277cdff5eb7e05fe1c90f
Samuel Fabrizi <samuel.fabrizi97@gmail.com>
2021-10-03 11:38:24 +0200

    doc: added documentation for models' read/write functions

--- decentralized_smart_grid_ml/federated_learning/models_reader_writer.py
+++ decentralized_smart_grid_ml/federated_learning/models_reader_writer.py
@@ -1,3 +1,6 @@
+"""
+Utility to save and load models' config and weights
+"""
 import json
 import pathlib
 
@@ -11,17 +14,34 @@ logger = create_logger(__name__)
 
 
 def save_fl_model(model, model_path):
+    """
+    Saves the whole model in a given path
+    :param model: model to save
+    :param model_path: directory path in which the model will be saved
+    :return:
+    """
     model.save(model_path)
     logger.info("Model saved in directory %s", model_path)
 
 
 def load_fl_model(model_path):
+    """
+    Loads a whole model from given path
+    :param model_path: directory path in which the model has been saved
+    :return: loaded model
+    """
     model = tf_models.load_model(model_path)
     logger.info("Load model from  %s", model_path)
     return model
 
 
 def save_fl_model_config(model, model_path):
+    """
+    Saves the configuration of the model in a json file
+    :param model: model that ccontains the config to save
+    :param model_path: file path in which the model's config will be saved
+    :return:
+    """
     if pathlib.Path(model_path).suffix != ".json":
         logger.error("The file path %s is not a json file", model_path)
         raise IncorrectExtensionFileError()
@@ -32,6 +52,11 @@ def save_fl_model_config(model, model_path):
 
 
 def load_fl_model_config(model_path):
+    """
+    Loads a model's config
+    :param model_path: file path in which the model's config has been saved
+    :return: loaded model's config
+    """
     if pathlib.Path(model_path).suffix != ".json":
         logger.error("The file path %s is not a json file", model_path)
         raise IncorrectExtensionFileError()
@@ -43,6 +68,12 @@ def load_fl_model_config(model_path):
 
 
 def save_fl_model_weights(model, model_weights_path):
+    """
+    Saves the model's weights in a json file
+    :param model: model that contains the weights to save
+    :param model_weights_path: file path in which the model's weights will be saved
+    :return:
+    """
     if pathlib.Path(model_weights_path).suffix != ".json":
         logger.error("The file path %s is not a json file", model_weights_path)
         raise IncorrectExtensionFileError()
@@ -54,6 +85,11 @@ def save_fl_model_weights(model, model_weights_path):
 
 
 def load_fl_model_weights(model_weights_path):
+    """
+    Loads the model's weights
+    :param model_weights_path: file path in which the model's weights has been saved
+    :return: loaded model's weights
+    """
     if pathlib.Path(model_weights_path).suffix != ".json":
         logger.error("The file path %s is not a json file", model_weights_path)
         raise IncorrectExtensionFileError()
@@ -62,4 +98,3 @@ def load_fl_model_weights(model_weights_path):
     logger.info("Loaded model's weights from %s", model_weights_path)
     loaded_model_weights = [np.array(layer_weights) for layer_weights in model_weights]
     return loaded_model_weights
-
