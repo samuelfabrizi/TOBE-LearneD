f1cda69d16bc63bfd1f0a9c67a1b2d01e2e04341
Samuel Fabrizi <samuel.fabrizi97@gmail.com>
2021-10-03 12:51:14 +0200

    fix: updated linter script

--- linter_script.sh
+++ linter_script.sh
@@ -3,4 +3,4 @@
 pip install -r requirements/requirements.txt
 pip install -r requirements/requirements.txt
 
-pylint --fail-under=9 --ignore-patterns=test* --disable=W1514 decentralized_smart_grid_ml/federated_learning
+pylint --fail-under=9 --ignore-patterns=test* --disable=W1514 decentralized_smart_grid_ml/
