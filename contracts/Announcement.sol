f9b6ccb2122f97a5907010a61da7f86d93d29813
Samuel Fabrizi <samuel.fabrizi97@gmail.com>
2021-10-07 17:54:19 +0200

    doc: added documentation for Announcement

--- contracts/Announcement.sol
+++ contracts/Announcement.sol
@@ -1,32 +1,48 @@
 pragma solidity >=0.4.22 <0.9.0;
 
+/// @title Announcement
+/// @notice This contract contains all the information related to the announcement
+///         of a ML task proposed
 contract Announcement {
 
+  /// @title ParticipantSubscription
+  /// @notice This struct contains the participants's information
   struct ParticipantSubscription {
     bool isSubscribed;
     bool[] rounds;
   }
 
+  // address of the manufacturer
   address public manufacturerAddress;
+  // name of the task
   bytes32 public taskName;
   // we can move the description in a decentralized storage
   // if it requires too much gas
+  // description of the task
   bytes32 public taskDescription;
   // TODO: for now the deadlineDate is only the number of days
+  // date of the deadline
   uint256 public deadlineDate;
+  // directory path to the whole model (both config and weights)
   bytes32 public modelArtifact;
   // this field is only to show a human-readable model's config
+  // file path to model's config
   bytes32 public modelConfig;
   // actually this is not used by the participants, it is contained
   // in modelArtifact
+  // file path to model's weights
   bytes32 public modelWeights;
+  // number of rounds for the federated learning
   uint8 public flRound;
   mapping(address => ParticipantSubscription) public participants;
 
+  /// @notice Sets the manufacturer address
   constructor () public {
     manufacturerAddress = msg.sender;
   }
 
+  /// @notice Check if the sender address corresponds
+  ///         to the manufacturer address
   modifier onlyManufacturer() {
     require(
       msg.sender == manufacturerAddress,
@@ -35,6 +51,15 @@ contract Announcement {
     _;
   }
 
+  /// @notice Initializes the announcement
+  /// @param _taskName name of the task
+  /// @param _description description of the task
+  /// @param _deadlineDate date of the deadline
+  /// @param _modelArtifact directory path to the whole model
+  ///        (both config and weights)
+  /// @param _modelConfig file path to model's config
+  /// @param _modelWeights file path to model's weights
+  /// @param _flRound number of rounds for the federated learning
   function initialize (
     bytes32 _taskName,
     bytes32 _taskDescription,
@@ -54,6 +79,7 @@ contract Announcement {
     flRound = _flRound;
   }
 
+  /// @notice Subscribes the sender in the announcement
   function subscribe() public {
     participants[msg.sender] = ParticipantSubscription(
       true,
@@ -61,10 +87,11 @@ contract Announcement {
     );
   }
 
-
+  /// @notice Checks if the sender is subscribed
+  /// @return True if the sendes is subscribed
+  ///         False otherwise
   function isSubscribed() public view returns(bool) {
-    return participants[msg.sender].isSubscribed; 
+    return participants[msg.sender].isSubscribed;
   }
-
 
 }
