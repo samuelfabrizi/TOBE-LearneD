0aca4aeea6d8246038de148f6b958ff46174657c
Samuel Fabrizi <samuel.fabrizi97@gmail.com>
2021-10-07 15:32:13 +0200

    refactor: added describe for the sake of clarity

--- tests_contracts/test_announcement.js
+++ tests_contracts/test_announcement.js
@@ -11,63 +11,71 @@ const flRound = 5;
 
 contract("Test Announcement smart contract", accounts => {
   const manufacturer = accounts[0];
+  const consumer1 = accounts[1];
+  const consumer2 = accounts[2];
+
   beforeEach('Deploy the Announcement smart contract', async () => {
     announcementInstance = await Announcement.deployed({from: manufacturer});
   });
 
-  it("should deploy the Announcement SC", async () => {
+  describe("Announcement SC initialization", async () => {
+
+    it("shoud deploy the Announcement SC", async () => {
     assert.equal(
       await announcementInstance.manufacturerAddress(),
       manufacturer,
       "The owner of the Announcement SC should be the manufacturer: " + manufacturer);
-  });
+    });
 
-  it("should initialize the Announcement SC with the ML task attributes", async () => {
-    await announcementInstance.initialize(
-      web3.utils.fromUtf8(taskName),
-      web3.utils.fromUtf8(taskDescription),
-      deadlineDate,
-      web3.utils.fromUtf8(modelArtifact),
-      web3.utils.fromUtf8(modelConfig),
-      web3.utils.fromUtf8(modelWeights),
-      flRound,
-      {from: manufacturer}
-    );
-    assert.equal(
-      web3.utils.toUtf8(await announcementInstance.taskName()),
-      taskName,
-      "The task name should be " + manufacturer
-    );
-    assert.equal(
-      web3.utils.toUtf8(await announcementInstance.taskDescription()),
-      taskDescription,
-      "The task description should be " + manufacturer
-    );
-    assert.equal(
-      await announcementInstance.deadlineDate(),
-      deadlineDate,
-      "The task deadline should be " + manufacturer
-    );
-    assert.equal(
-      web3.utils.toUtf8(await announcementInstance.modelArtifact()),
-      modelArtifact,
-      "The model's artifact should be " + manufacturer
-    );
-    assert.equal(
-      web3.utils.toUtf8(await announcementInstance.modelConfig()),
-      modelConfig,
-      "The model's config should be " + manufacturer
-    );
-    assert.equal(
-      web3.utils.toUtf8(await announcementInstance.modelWeights()),
-      modelWeights,
-      "The model's weights should be " + manufacturer
-    );
-    assert.equal(
-      await announcementInstance.flRound(),
-      flRound,
-      "The federated rounds shold be " + manufacturer
-    );
+
+    it("should initialize the Announcement SC with the ML task attributes", async () => {
+      await announcementInstance.initialize(
+        web3.utils.fromUtf8(taskName),
+        web3.utils.fromUtf8(taskDescription),
+        deadlineDate,
+        web3.utils.fromUtf8(modelArtifact),
+        web3.utils.fromUtf8(modelConfig),
+        web3.utils.fromUtf8(modelWeights),
+        flRound,
+        {from: manufacturer}
+      );
+      assert.equal(
+        web3.utils.toUtf8(await announcementInstance.taskName()),
+        taskName,
+        "The task name should be " + manufacturer
+      );
+      assert.equal(
+        web3.utils.toUtf8(await announcementInstance.taskDescription()),
+        taskDescription,
+        "The task description should be " + manufacturer
+      );
+      assert.equal(
+        await announcementInstance.deadlineDate(),
+        deadlineDate,
+        "The task deadline should be " + manufacturer
+      );
+      assert.equal(
+        web3.utils.toUtf8(await announcementInstance.modelArtifact()),
+        modelArtifact,
+        "The model's artifact should be " + manufacturer
+      );
+      assert.equal(
+        web3.utils.toUtf8(await announcementInstance.modelConfig()),
+        modelConfig,
+        "The model's config should be " + manufacturer
+      );
+      assert.equal(
+        web3.utils.toUtf8(await announcementInstance.modelWeights()),
+        modelWeights,
+        "The model's weights should be " + manufacturer
+      );
+      assert.equal(
+        await announcementInstance.flRound(),
+        flRound,
+        "The federated rounds should be " + manufacturer
+      );
+
+    });
 
   });
 
