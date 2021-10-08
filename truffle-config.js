1cdd106bc8f5d2d8fc8ee715806a9d1b2532e522
Samuel Fabrizi <samuel.fabrizi97@gmail.com>
2021-10-08 13:04:31 +0200

    ci: added ganache network, restored test netowork for local tests

--- truffle-config.js
+++ truffle-config.js
@@ -41,7 +41,7 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    development: {
+    ganache: {
         host: "127.0.0.1",     // Localhost (default: none)
         port: 7545,            // Ganache port
         network_id: "*",       // Any network (default: none)
@@ -100,7 +100,7 @@ module.exports = {
   // NOTE: It is not possible to migrate your contracts to truffle DB and you should
   // make a backup of your artifacts to a safe location before enabling this feature.
   //
-  // After you backed up your artifacts you can utilize db by running migrate as follows: 
+  // After you backed up your artifacts you can utilize db by running migrate as follows:
   // $ truffle migrate --reset --compile-all
   //
   // db: {
