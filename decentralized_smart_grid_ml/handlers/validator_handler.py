d5e2d76729ffbc06fdc9c553a32a5e13fc8c5aec
Samuel Fabrizi <samuel.fabrizi97@gmail.com>
2021-10-13 23:48:21 +0200

    doc: added documentation for validator handler

--- decentralized_smart_grid_ml/handlers/validator_handler.py
+++ decentralized_smart_grid_ml/handlers/validator_handler.py
@@ -10,12 +10,24 @@ logger = create_logger(__name__)
 
 
 class ValidatorHandler(FileSystemEventHandler):
+    """ This class is responsible to trigger Aggregator actions when a new file is created
+    in a given path """
 
     def __init__(self, aggregator):
+        """
+        Initializes the handler
+        :param aggregator: instance of Aggregator
+        """
         self.aggregator = aggregator
         super().__init__()
 
     def on_created(self, event):
+        """
+        Triggers an update in the aggregator
+        :param event: event generated
+        :return:
+        """
+        logger.info("The file %s has been created", event.src_path)
         round_is_completed = self.aggregator.add_participant_weights(event.src_path)
         if round_is_completed:
             self.aggregator.update_global_model()
