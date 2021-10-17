819533defa9a5785f4fe468c91816e5199215dc3
Samuel Fabrizi <samuel.fabrizi97@gmail.com>
2021-10-17 14:32:55 +0200

    feat: added local training for each round

--- decentralized_smart_grid_ml/federated_learning/federated_local_trainer.py
+++ decentralized_smart_grid_ml/federated_learning/federated_local_trainer.py
@@ -1,9 +1,16 @@
 """
 This module contains the class responsible for the training of the local participant's model
 """
+import os
+from pathlib import Path
+
 import pandas as pd
 
-from decentralized_smart_grid_ml.federated_learning.models_reader_writer import load_fl_model
+from decentralized_smart_grid_ml.federated_learning.models_reader_writer import load_fl_model, load_fl_model_weights, \
+    save_fl_model_weights
+from decentralized_smart_grid_ml.utils.bcai_logging import create_logger
+
+logger = create_logger(__name__)
 
 
 class FederatedLocalTrainer:
@@ -13,7 +20,7 @@ class FederatedLocalTrainer:
                  train_set_path, epochs, local_model_weights_path):
         self.participant_id = participant_id
         self.n_fl_rounds = n_fl_rounds
-        self.global_model_path = load_fl_model(global_model_path)
+        self.local_model = load_fl_model(global_model_path)
         train_set_df = pd.read_csv(train_set_path)
         # TODO: generalize this function to extract features and labels from the dataset
         self.x_train, self.y_train = train_set_df[["x1", "x2"]].values, train_set_df["y"].values
@@ -31,3 +38,43 @@ class FederatedLocalTrainer:
         for idx_round in range(self.n_fl_rounds):
             self.rounds2history[idx_round] = None
 
+    def fit_local_model(self, path_file_created):
+        history = None
+        if path_file_created is not None:
+            # try to load the aggregated new baseline model and start the local training
+            round_baseline_model = Path(path_file_created).stem[-1]
+            try:
+                if int(round_baseline_model) == self.current_round:
+                    aggregated_weights = load_fl_model_weights(path_file_created)
+                    # update the weights of the new baseline model for this round
+                    self.local_model.set_weights(aggregated_weights)
+                    # fit the local model
+                    history = self.local_model.fit(self.x_train, self.y_train, epochs=self.epochs)
+                else:
+                    logger.warning(
+                        "The path %s does not correspond to the current round (%d), Skipping...",
+                        path_file_created, self.current_round
+                    )
+            except ValueError:
+                logger.warning(
+                    "Malformed path %s, Skipping...",
+                    path_file_created
+                )
+        else:
+            # first round: the baseline model is in global_model_path
+            history = self.local_model.fit(self.x_train, self.y_train, epochs=self.epochs)
+        if history is not None:
+            local_model_weights_path = os.path.join(
+                self.local_model_weights_path,
+                "weights_round_" + str(self.current_round) + ".json"
+            )
+            output_folder = Path(self.local_model_weights_path)
+            output_folder.mkdir(parents=True, exist_ok=True)
+            self.rounds2history[self.current_round] = history
+            self.current_round += 1
+            save_fl_model_weights(self.local_model, local_model_weights_path)
+            logger.info("Client %s: end FL round %s", self.participant_id, self.current_round)
+        if self.current_round == self.n_fl_rounds:
+            return True
+        else:
+            return False
--- decentralized_smart_grid_ml/tests/federated_learning/test_federated_local_trainer.py
+++ decentralized_smart_grid_ml/tests/federated_learning/test_federated_local_trainer.py
@@ -1,3 +1,4 @@
+import pathlib
 import unittest
 from unittest.mock import patch
 
@@ -10,8 +11,8 @@ class TestFederatedLocalTrainer(unittest.TestCase):
 
     @patch("pandas.read_csv")
     @patch("decentralized_smart_grid_ml.federated_learning.federated_local_trainer.load_fl_model")
-    def test_aggregator_constructor(self, load_fl_model_mock, read_csv_mock):
-        client_id = 0
+    def test_federated_local_trainer_constructor(self, load_fl_model_mock, read_csv_mock):
+        participant_id = 0
         n_fl_rounds = 2
         global_model_path = "/path/to/model"
         train_set_path = "/path/to/train.csv"
@@ -22,14 +23,14 @@ class TestFederatedLocalTrainer(unittest.TestCase):
             "x2": [1, 2],
             "y": [0, 1]
         })
-        model_artifact = "expected model"
+        model_artifact = "expected local model"
         load_fl_model_mock.return_value = model_artifact
         rounds2history_expected = {
             0: None,
             1: None
         }
         flt = FederatedLocalTrainer(
-            client_id,
+            participant_id,
             n_fl_rounds,
             global_model_path,
             train_set_path,
@@ -38,4 +39,170 @@ class TestFederatedLocalTrainer(unittest.TestCase):
         )
         read_csv_mock.assert_called_with(train_set_path)
         load_fl_model_mock.assert_called_with(global_model_path)
+        self.assertEqual(model_artifact, flt.local_model)
+        self.assertEqual(0, flt.current_round)
         self.assertDictEqual(rounds2history_expected, flt.rounds2history)
+
+    @patch.object(pathlib.Path, 'mkdir')
+    @patch("tensorflow.keras.Sequential")
+    @patch("decentralized_smart_grid_ml.federated_learning.federated_local_trainer.save_fl_model_weights")
+    @patch("decentralized_smart_grid_ml.federated_learning.federated_local_trainer.load_fl_model_weights")
+    @patch(
+        "decentralized_smart_grid_ml.federated_learning.federated_local_trainer.FederatedLocalTrainer.__init__",
+        return_value=None
+    )
+    def test_fit_local_model_completed(self, federated_local_trainer_mock, load_fl_model_weights_mock,
+                                       save_fl_model_weights_mock, local_model_mock, mkdir_mock):
+        current_round = 1
+        n_fl_rounds = 2
+        path_file_created = "validator/validator_weights_round_1.json"
+        baseline_model_weights = "baseline model weights"
+        x_train = "train x"
+        y_train = "train y"
+        n_epochs = 5
+        local_model_weights_path = "clients/client_0/"
+        local_model_trained_path = local_model_weights_path + "weights_round_" + str(current_round) + ".json"
+        load_fl_model_weights_mock.return_value = baseline_model_weights
+        expected_history = "history 1"
+        rounds2history_expected = {
+            0: None,
+            1: expected_history
+        }
+        local_model_mock.fit.return_value = expected_history
+        flt = FederatedLocalTrainer()
+        flt.x_train = x_train
+        flt.y_train = y_train
+        flt.epochs = n_epochs
+        flt.current_round = current_round
+        flt.local_model = local_model_mock
+        flt.participant_id = 0
+        flt.n_fl_rounds = n_fl_rounds
+        flt.local_model_weights_path = local_model_weights_path
+        flt.rounds2history = {
+            0: None,
+            1: None
+        }
+        is_completed = flt.fit_local_model(path_file_created)
+        local_model_mock.fit.assert_called_with(x_train, y_train, epochs=n_epochs)
+        local_model_mock.set_weights.assert_called_with(baseline_model_weights)
+        save_fl_model_weights_mock.assert_called_with(local_model_mock, local_model_trained_path)
+        self.assertDictEqual(rounds2history_expected, flt.rounds2history)
+        self.assertEqual(True, is_completed)
+
+    @patch.object(pathlib.Path, 'mkdir')
+    @patch("tensorflow.keras.Sequential")
+    @patch("decentralized_smart_grid_ml.federated_learning.federated_local_trainer.save_fl_model_weights")
+    @patch("decentralized_smart_grid_ml.federated_learning.federated_local_trainer.load_fl_model_weights")
+    @patch(
+        "decentralized_smart_grid_ml.federated_learning.federated_local_trainer.FederatedLocalTrainer.__init__",
+        return_value=None
+    )
+    def test_fit_local_model_not_completed(self, federated_local_trainer_mock, load_fl_model_weights_mock,
+                                           save_fl_model_weights_mock, local_model_mock, mkdir_mock):
+        current_round = 1
+        n_fl_rounds = 3
+        path_file_created = "validator/validator_weights_round_1.json"
+        baseline_model_weights = "baseline model weights"
+        x_train = "train x"
+        y_train = "train y"
+        n_epochs = 5
+        local_model_weights_path = "clients/client_0/"
+        local_model_trained_path = local_model_weights_path + "weights_round_" + str(current_round) + ".json"
+        load_fl_model_weights_mock.return_value = baseline_model_weights
+        expected_history = "history 1"
+        rounds2history_expected = {
+            0: None,
+            1: expected_history,
+            2: None,
+        }
+        local_model_mock.fit.return_value = expected_history
+        flt = FederatedLocalTrainer()
+        flt.x_train = x_train
+        flt.y_train = y_train
+        flt.epochs = n_epochs
+        flt.current_round = current_round
+        flt.local_model = local_model_mock
+        flt.participant_id = 0
+        flt.n_fl_rounds = n_fl_rounds
+        flt.local_model_weights_path = local_model_weights_path
+        flt.rounds2history = {
+            0: None,
+            1: None,
+            2: None
+        }
+        is_completed = flt.fit_local_model(path_file_created)
+        local_model_mock.fit.assert_called_with(x_train, y_train, epochs=n_epochs)
+        local_model_mock.set_weights.assert_called_with(baseline_model_weights)
+        save_fl_model_weights_mock.assert_called_with(local_model_mock, local_model_trained_path)
+        self.assertEqual(False, is_completed)
+        self.assertDictEqual(rounds2history_expected, flt.rounds2history)
+
+    @patch.object(pathlib.Path, 'mkdir')
+    @patch("tensorflow.keras.Sequential")
+    @patch("decentralized_smart_grid_ml.federated_learning.federated_local_trainer.save_fl_model_weights")
+    @patch(
+        "decentralized_smart_grid_ml.federated_learning.federated_local_trainer.FederatedLocalTrainer.__init__",
+        return_value=None
+    )
+    def test_fit_local_model_first_round(self, federated_local_trainer_mock, save_fl_model_weights_mock,
+                                         local_model_mock, mkdir_mock):
+        current_round = 0
+        n_fl_rounds = 2
+        x_train = "train x"
+        y_train = "train y"
+        n_epochs = 5
+        local_model_weights_path = "clients/client_0/"
+        local_model_trained_path = local_model_weights_path + "weights_round_" + str(current_round) + ".json"
+        expected_history = "history 1"
+        rounds2history_expected = {
+            0: expected_history,
+            1: None
+        }
+        local_model_mock.fit.return_value = expected_history
+        flt = FederatedLocalTrainer()
+        flt.x_train = x_train
+        flt.y_train = y_train
+        flt.epochs = n_epochs
+        flt.current_round = current_round
+        flt.local_model = local_model_mock
+        flt.participant_id = 0
+        flt.n_fl_rounds = n_fl_rounds
+        flt.local_model_weights_path = local_model_weights_path
+        flt.rounds2history = {
+            0: None,
+            1: None
+        }
+        # we pass None because it is the first round
+        is_completed = flt.fit_local_model(None)
+        local_model_mock.fit.assert_called_with(x_train, y_train, epochs=n_epochs)
+        save_fl_model_weights_mock.assert_called_with(local_model_mock, local_model_trained_path)
+        self.assertEqual(False, is_completed)
+        self.assertDictEqual(rounds2history_expected, flt.rounds2history)
+
+    @patch(
+        "decentralized_smart_grid_ml.federated_learning.federated_local_trainer.FederatedLocalTrainer.__init__",
+        return_value=None
+    )
+    def test_fit_local_model_malformed_path(self, federated_local_trainer_mock):
+        current_round = 1
+        path_file_created = "validator/validator_weights_round_null.json"
+        flt = FederatedLocalTrainer()
+        flt.current_round = current_round
+        flt.n_fl_rounds = 2
+        flt.participant_id = 0
+        is_completed = flt.fit_local_model(path_file_created)
+        self.assertEqual(False, is_completed)
+
+    @patch(
+        "decentralized_smart_grid_ml.federated_learning.federated_local_trainer.FederatedLocalTrainer.__init__",
+        return_value=None
+    )
+    def test_fit_local_model_wront_round(self, federated_local_trainer_mock):
+        current_round = 1
+        path_file_created = "validator/validator_weights_round_0.json"
+        flt = FederatedLocalTrainer()
+        flt.current_round = current_round
+        flt.n_fl_rounds = 2
+        flt.participant_id = 0
+        is_completed = flt.fit_local_model(path_file_created)
+        self.assertEqual(False, is_completed)
