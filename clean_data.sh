8f9ab5d82ebe9f8221531fb98f2b692d4425852e
Samuel Fabrizi <samuel.fabrizi97@gmail.com>
2021-10-24 15:35:08 +0200

    fix: modified file names to remove

--- clean_data.sh
+++ clean_data.sh
@@ -1,3 +1,4 @@
-find ./data_sample -type f -name '*.json' -delete
+find ./data_sample/simple_ml_task -type f -name '*weights*' -delete
+find ./data_sample/simple_ml_task -type f -name '*config*' -delete
 rm -rf data_sample/simple_ml_task/linear_model
 rm announcement_info.json
