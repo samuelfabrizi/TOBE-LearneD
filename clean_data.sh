1d1e5c0a6a3026b508f35ef99af49f06f0d75b91
Samuel Fabrizi <samuel.fabrizi97@gmail.com>
2021-10-25 18:19:48 +0200

    feat: added local datasets removal

--- clean_data.sh
+++ clean_data.sh
@@ -1,4 +1,6 @@
 find ./data_sample/simple_ml_task -type f -name '*weights*' -delete
+find ./data_sample/simple_ml_task/participants -type f -name '*.csv' -delete
+find ./data_sample/simple_ml_task/validator -type f -name '*.csv' -delete
 rm -rf data_sample/simple_ml_task/linear_model
 rm -rf data_sample/simple_ml_task/linear_model_config.json
 rm announcement_info.json
