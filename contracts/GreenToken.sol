afdc472ac22eac25a03adc70363403aa34bb5f92
Samuel Fabrizi <samuel.fabrizi97@gmail.com>
2021-11-07 16:27:02 +0100

    fix: changed initial supply

--- contracts/GreenToken.sol
+++ contracts/GreenToken.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol"
 contract GreenToken is ERC20PresetMinterPauser{
 
   constructor() ERC20PresetMinterPauser("Green Token", "GRN") {
-    _mint(msg.sender, 1000000 * (10 ** uint256(decimals())));
+    _mint(msg.sender, 1000000000000000000);
   }
 
 }
