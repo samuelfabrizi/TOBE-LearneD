3a91f170f3e62fa92787afe513d151348409eabf
Samuel Fabrizi <samuel.fabrizi97@gmail.com>
2021-11-07 17:01:56 +0100

    feat: added tokens purchase by manufacturer

--- scripting/create_announcement.py
+++ scripting/create_announcement.py
@@ -12,7 +12,8 @@ from decentralized_smart_grid_ml.contract_interactions.announcement_configuratio
 from decentralized_smart_grid_ml.federated_learning.models_reader_writer import save_fl_model, \
     save_fl_model_config, save_fl_model_weights
 from decentralized_smart_grid_ml.utils.bcai_logging import create_logger
-from decentralized_smart_grid_ml.utils.config import BLOCKCHAIN_ADDRESS, ANNOUNCEMENT_JSON_PATH, get_address_contract
+from decentralized_smart_grid_ml.utils.config import BLOCKCHAIN_ADDRESS, ANNOUNCEMENT_JSON_PATH, \
+    get_addresses_contracts, DEX_JSON_PATH
 
 logger = create_logger(__name__)
 
@@ -62,7 +63,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
     logger.info("Starting script to initialize an announcement")
 
-    contract_address = get_address_contract(args.contract_info_path)
+    announcement_contract_address, dex_contract_address = get_addresses_contracts(args.contract_info_path)
     announcement_config = AnnouncementConfiguration.read_json_config(args.task_config_path)
 
     # load test set
@@ -91,14 +92,28 @@ if __name__ == '__main__':
         contract_json = json.load(file)  # load contract info as JSON
         contract_abi = contract_json['abi']  # fetch contract's abi - necessary to call its functions
 
-    # Fetch deployed contract reference
-    contract = web3.eth.contract(address=contract_address, abi=contract_abi)
-    logger.info("Fetched contract %s", contract_address)
+    with open(DEX_JSON_PATH) as file:
+        dex_contract_json = json.load(file)  # load contract info as JSON
+        dex_contract_abi = dex_contract_json['abi']  # fetch contract's abi - necessary to call its functions
+
+    # Fetch deployed Announcement contract reference
+    announcement_contract = web3.eth.contract(address=announcement_contract_address, abi=contract_abi)
+    logger.info("Fetched announcement contract from %s", announcement_contract_address)
+
+    # Fetch deployed GreenDEX contract reference
+    dex_contract = web3.eth.contract(address=dex_contract_address, abi=dex_contract_abi)
+    logger.info("Fetched GreenDEX contract from %s", dex_contract_address)
 
     # automatically takes the first address
     manufacturer_address = web3.eth.accounts[0]
+
+    dex_contract.functions.buy().transact({
+        'from': manufacturer_address,
+        'value': args.n_tokens_at_stake
+    })
+
     # Call contract function (this is not persisted to the blockchain)
-    contract.functions.initialize(
+    announcement_contract.functions.initialize(
         args.task_config_path,              # path to the configuration file of the task
         args.max_number_participants,       # maximum number of participants,
         args.n_tokens_at_stake              # number of tokens at stake
