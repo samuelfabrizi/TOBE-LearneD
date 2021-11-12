5cf1a97a67e30e9f25351a2e65210358b6e75ae3
Samuel Fabrizi <samuel.fabrizi97@gmail.com>
2021-11-12 18:41:46 +0100

    fix: added solidity coverage output cleaning , added comments

--- clean_data.sh
+++ clean_data.sh
@@ -1,6 +1,14 @@
+# delete the weights
 find ./data_sample/simple_ml_task -type f -name '*weights*' -delete
+# delete the participants directories
 find ./data_sample/simple_ml_task/participants -type f -name '*.csv' -delete
+# delete the datasets
 find ./data_sample/simple_ml_task/validator -type f -name '*.csv' -delete
+# delete the models
 rm -rf data_sample/simple_ml_task/linear_model
 rm -rf data_sample/simple_ml_task/linear_model_config.json
-rm announcement_info.json
+# delete the announcement info
+rm -f announcement_info.json
+# delete the files created by solidity-coverage
+rm -rf coverage
+rm -f coverage.json
