147995dea92629ebf6ab92a10d88928c16100590
Samuel Fabrizi <samuel.fabrizi97@gmail.com>
2021-11-12 18:46:57 +0100

    feat: merge soldity and python linting and coverage

--- coverage_test.sh
+++ coverage_test.sh
@@ -1,7 +1,9 @@
 # Script used for coverage test on python modules
 
-pip install -r requirements/requirements.txt
 pip install -r requirements/requirements-test.txt
 
 coverage run -a --rcfile=./.coveragerc -m unittest
 coverage report --fail-under=90
+
+truffle run coverage
+sh clean_data.sh
