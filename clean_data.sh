1d767515fb8b8495b40fba3a40231ce31b038e7f
Samuel Fabrizi <samuel.fabrizi97@gmail.com>
2021-12-19 18:04:05 +0100

    style: levelled out models' names

--- clean_data.sh
+++ clean_data.sh
@@ -1,45 +1,37 @@
 #### delete simple ml task results ####
 
-# delete the weights
-find ./data_sample/simple_ml_task -type f -name '*weights*' -delete
-# delete the datasets
-find ./data_sample/simple_ml_task/validator -type f -name '*.csv' -delete
-find ./data_sample/simple_ml_task/participants -type f -name '*.csv' -delete
-# delete the participants statistics
-find ./data_sample/simple_ml_task/participants -type f -name '*statistics*' -delete
-# delete the validator statistics
-find ./data_sample/simple_ml_task/validator -type f -name '*statistics*' -delete
-# delete the models
-rm -rf data_sample/simple_ml_task/linear_model
-rm -rf data_sample/simple_ml_task/linear_model_config.json
 
+# delete the participants' directory
+rm -rf ./data_sample/simple_ml_task/participants
+# delete the validator's directory
+rm -rf ./data_sample/simple_ml_task/validator
+# delete models info
+rm -rf data_sample/simple_ml_task/model
+rm -rf data_sample/simple_ml_task/model_config.json
+rm -rf data_sample/simple_ml_task/model_weights
 
 #### delete simple multi-classification task results ####
 
-# delete the weights
-find ./data_sample/simple_multiclass_task -type f -name '*weights*' -delete
-# delete the datasets
-find ./data_sample/simple_multiclass_task/validator -type f -name '*.csv' -delete
-find ./data_sample/simple_multiclass_task/participants -type f -name '*.csv' -delete
-# delete the participants statistics
-find ./data_sample/simple_multiclass_task/participants -type f -name '*statistics*' -delete
-# delete the validator statistics
-find ./data_sample/simple_multiclass_task/validator -type f -name '*statistics*' -delete
-# delete the models
+# delete the participants' directory
+rm -rf ./data_sample/simple_multiclass_task/participants
+# delete the validator's directory
+rm -rf ./data_sample/simple_multiclass_task/validator
+# delete models info
 rm -rf data_sample/simple_multiclass_task/model
 rm -rf data_sample/simple_multiclass_task/model_config.json
+rm -rf data_sample/simple_multiclass_task/model_weights
+
 
 #### delete appliance classification task results ####
 
-# delete the weights
-find ./data_sample/appliance_classification_task -type f -name '*weights*' -delete
-# delete the participants directories
-# find ./data_sample/appliance_classification_task/participants -type f -name '*.csv' -delete
-# delete the datasets
-#find ./data_sample/appliance_classification_task/validator -type f -name '*.csv' -delete
-# delete the models
+# delete the participants' directory
+rm -rf ./data_sample/appliance_classification_task/participants
+# delete the validator's directory
+rm -rf ./data_sample/appliance_classification_task/validator
+# delete models info
 rm -rf data_sample/appliance_classification_task/model
 rm -rf data_sample/appliance_classification_task/model_config.json
+rm -rf data_sample/appliance_classification_task/model_weights
 
 
 # delete the announcement info
