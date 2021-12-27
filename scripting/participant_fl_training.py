124dc47cca4e5c0d6bb2dd99538b0c7287906516
Samuel Fabrizi <samuel.fabrizi97@gmail.com>
2021-12-27 17:17:18 +0100

    feat: added fake weights for malicious participants

--- scripting/participant_fl_training.py
+++ scripting/participant_fl_training.py
@@ -6,19 +6,70 @@ import json
 import os
 import sys
 import time
+from pathlib import Path
 
+from numpy.random import rand
 from web3 import HTTPProvider, Web3
 
 from watchdog.observers import Observer
 
 from decentralized_smart_grid_ml.contract_interactions.announcement_configuration import AnnouncementConfiguration
 from decentralized_smart_grid_ml.federated_learning.federated_local_trainer import FederatedLocalTrainer
+from decentralized_smart_grid_ml.federated_learning.models_reader_writer import load_fl_model_weights, \
+    save_fl_model_weights
 from decentralized_smart_grid_ml.handlers.participant_handler import ParticipantHandler
 from decentralized_smart_grid_ml.utils.bcai_logging import create_logger
 from decentralized_smart_grid_ml.utils.config import BLOCKCHAIN_ADDRESS, ANNOUNCEMENT_JSON_PATH, \
     get_addresses_contracts, PROJECT_ABSOLUTE_PATH
 
 logger = create_logger(__name__)
+
+
+class FakeFederatedLocalTrainer(FederatedLocalTrainer):
+    """ This class is responsible for the creation of fake weights for the
+    local participant's model """
+
+    def fit_local_model(self, path_file_created):
+        if path_file_created is not None:
+            # try to load the aggregated new baseline model and start the local training
+            round_baseline_model = Path(path_file_created).stem[-1]
+            try:
+                if int(round_baseline_model) == self.current_round:
+                    aggregated_weights = load_fl_model_weights(path_file_created)
+                    fake_weights = []
+                    for weights in aggregated_weights:
+                        fake_weights.append(rand(*weights.shape))
+                    self.local_model.set_weights(fake_weights)
+                else:
+                    logger.warning(
+                        "The path %s does not correspond to the current round (%d), Skipping...",
+                        path_file_created, self.current_round
+                    )
+            except ValueError:
+                logger.warning(
+                    "Malformed path %s, Skipping...",
+                    path_file_created
+                )
+                return self.is_finished
+        else:
+            # first round: the baseline model is in global_model_path
+            aggregated_weights = self.local_model.get_weights()
+            fake_weights = []
+            for weights in aggregated_weights:
+                fake_weights.append(rand(*weights.shape))
+            self.local_model.set_weights(fake_weights)
+        local_model_weights_path = os.path.join(
+            self.local_model_weights_path,
+            "weights_round_" + str(self.current_round) + ".json"
+        )
+        output_folder = Path(self.local_model_weights_path)
+        output_folder.mkdir(parents=True, exist_ok=True)
+        self.rounds2history[self.current_round] = None
+        logger.info("Participant %s: end FL round %s", self.participant_id, self.current_round)
+        self.current_round += 1
+        save_fl_model_weights(self.local_model, local_model_weights_path)
+        self.is_finished = self.current_round == self.announcement_config.fl_rounds
+        return self.is_finished
 
 
 if __name__ == '__main__':
@@ -46,6 +97,13 @@ if __name__ == '__main__':
         type=str,
         help='The path to the directory that contains the aggregated baseline models for each round',
         required=True
+    )
+    parser.add_argument(
+        '--use_fake_weights',
+        dest='use_fake_weights',
+        action='store_true',
+        help="Flag used to indicates if you want to use fake weights",
+        default=False
     )
 
     args = parser.parse_args()
@@ -105,12 +163,21 @@ if __name__ == '__main__':
     time.sleep(2)
     logger.info("Starting participant federated learning")
 
-    federated_local_trainer = FederatedLocalTrainer(
-        participant_id,
-        announcement_configuration,
-        local_dataset_path,
-        participant_directory_path
-    )
+    if args.use_fake_weights:
+        federated_local_trainer = FakeFederatedLocalTrainer(
+            participant_id,
+            announcement_configuration,
+            local_dataset_path,
+            participant_directory_path
+        )
+    else:
+        federated_local_trainer = FederatedLocalTrainer(
+            participant_id,
+            announcement_configuration,
+            local_dataset_path,
+            participant_directory_path
+        )
+
     federated_local_trainer.fit_local_model(None)
 
     participant_handler = ParticipantHandler(federated_local_trainer)
